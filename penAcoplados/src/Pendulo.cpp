90bc0456662cae0a3bf6fc450e7a38d82b2911d1
leon9041 <66146734+leon9041@users.noreply.github.com>
2025-10-29 01:03:54 -0500

    Update Pendulo.cpp

--- penAcoplados/src/Pendulo.cpp
+++ penAcoplados/src/Pendulo.cpp
@@ -1,3 +1,11 @@
+/**
+ * @file     Pendulo.cpp
+ * @brief    implementacion de poo del pendulo
+ * @author   Angie Gomez, Leonardo Tovar
+ * @date     29/10/25
+ * @version  1.0
+ * @license  owner
+ */
 #include "Pendulo.h"
 
 Pendulo::Pendulo(double theta0, double omega0, double g_, double l_, double m_)
